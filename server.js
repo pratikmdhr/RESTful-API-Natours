ae6126fafdb04c8da37f3077c6571e314aa006d6
Pratik <pratikmndh@gmail.com>
2022-01-11 16:29:37 -0500

    Setup ESLint and MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
