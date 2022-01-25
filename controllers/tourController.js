1e981488c8d1fcce99116ca3846ac4365f112933
Pratik <pratikmndh@gmail.com>
2022-01-24 22:14:59 -0500

    Add createTour function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('./../models/tourModel')
+const Tour = require('./../models/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
@@ -15,16 +15,16 @@ const Tour = require('./../models/tourModel')
 //   next();
 // };
 
-exports.checkBody = (req, res, next) => {
-  console.log(req.body);
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'bad request',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   console.log(req.body);
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'bad request',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -45,22 +45,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+exports.createTour = async (req, res) => {
+
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
