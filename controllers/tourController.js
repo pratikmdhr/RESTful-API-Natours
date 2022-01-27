49fde5792318931054e68120126188f236282b04
Pratik <pratikmndh@gmail.com>
2022-01-26 22:23:28 -0500

    Add getAllTours and getTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,43 +1,35 @@
 const Tour = require('./../models/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`tour id is ${val}`);
-//   if (+req.params.id > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   console.log(req.body);
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'bad request',
-//     });
-//   }
-//   next();
-// };
-
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: { tours },
-  });
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 
   res.status(200).json({
     status: 'success',
@@ -46,7 +38,6 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-
   try {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
