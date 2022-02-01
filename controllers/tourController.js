ee455674b1b87b3a859856d51f5f7d48e8d5c6c2
Pratik <pratikmndh@gmail.com>
2022-01-31 20:15:31 -0500

    Add deleteTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,11 +30,6 @@ exports.getTour = async (req, res) => {
       message: err,
     });
   }
-
-  res.status(200).json({
-    status: 'success',
-    // data: { tour },
-  });
 };
 
 exports.createTour = async (req, res) => {
@@ -47,7 +42,7 @@ exports.createTour = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: 'Invalid data sent',
     });
@@ -67,16 +62,24 @@ exports.updateTour = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: err,
     });
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    // data: null,
-  });
+exports.deleteTour = async(req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
